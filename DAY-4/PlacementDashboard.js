138501a04d149fcfac25101d050c8dfd5ff1e800
Pallavinaidu2308 <bollampallavi0@gmail.com>
2026-08-07 14:06:46 +0530

    Add initial properties to PlacementDashboard component

--- DAY-4/PlacementDashboard.js
+++ DAY-4/PlacementDashboard.js
@@ -1,1 +1,18 @@
 
+import { LightningElement } from 'lwc';
+
+export default class PlacementDashboard extends LightningElement {
+
+    studentName = 'Bollam Pallavi';
+
+    todayDate = new Date().toLocaleDateString();
+
+    companies = 25;
+
+    jobs = 63;
+
+    applications = 5;
+
+    interviews = 2;
+
+}
