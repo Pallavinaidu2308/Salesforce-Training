b7cf700dec1048f472082f29b6ba41d2a326868e
Pallavinaidu2308 <bollampallavi0@gmail.com>
2026-08-07 14:09:45 +0530

    Add PlacementHome component with student details

--- DAY-4/PlacementHome.js
+++ DAY-4/PlacementHome.js
@@ -1,1 +1,20 @@
 
+import { LightningElement } from 'lwc';
+
+export default class PlacementHome extends LightningElement {
+
+    studentName = 'Bollam Pallavi';
+    rollNumber = '23PA1A5705';
+    department = 'CSBS';
+
+    message = '';
+    status = 'Not Applied';
+
+    showMessage() {
+        this.message = 'Welcome to Salesforce Development.';
+    }
+
+    applyJob() {
+        this.status = 'Applied';
+    }
+}
